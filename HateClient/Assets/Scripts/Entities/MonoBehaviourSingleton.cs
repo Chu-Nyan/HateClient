6c5a6041e19a230ec125466cb4ab77bada1ab1b4
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2025-06-09 20:56:18 +0900

    feat: 싱글톤, 풀링 추가

---
+++ HateClient/Assets/Scripts/Entities/MonoBehaviourSingleton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Library.DesignPattern
+{
+    public abstract class MonoBehaviourSingleton<T> : MonoBehaviour where T : MonoBehaviourSingleton<T>
+    {
+        public static T Instance;
+
+        protected virtual void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = (T)this;
+            }
+        }
+    }
+}
