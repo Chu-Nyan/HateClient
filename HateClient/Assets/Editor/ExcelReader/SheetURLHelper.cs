50626ff9b948cee2ae01d6514bdc0505d690bc82
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2025-06-25 14:45:54 +0900

    feat: 스크립트 파일 생성 함수 추가

--- HateClient/Assets/Editor/ExcelReader/SheetURLHelper.cs
+++ HateClient/Assets/Editor/ExcelReader/SheetURLHelper.cs
@@ -1,14 +1,42 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using ExcelDataReader;
+using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class SheetURLHelper : ScriptableObject
 {
-    [SerializeField, Header("구글시트 주소")]
-    private string _googleSheetID;
     private const string _googleDownloadURL = "https://docs.google.com/spreadsheets/d/{0}/export?format=xlsx";
+
+    [SerializeField, Header("구글 시트 ID")]
+    private string _googleSheetID;
+    [SerializeField, Header("변수 이름 행 번호")]
+    private int _dataNameRowNumber;
+    [SerializeField, Header("자료형 행 번호")]
+    private int _dataTypeRowNumber;
+    [SerializeField, Header("데이터 시작 행 번호")]
+    private int _dataStartedRowNumber;
+    [SerializeField, Header("클래스 생성 위치")]
+    private string _classGeneratedPath = Application.dataPath;
+
+    private int DataNameRowNumber
+    {
+        get => _dataNameRowNumber - 1;
+    }
+
+    private int DataTypeRowNumber
+    {
+        get => _dataTypeRowNumber - 1;
+    }
+
+    private int DataStartedRowNumber
+    {
+        get => _dataStartedRowNumber - 1;
+    }
 
     [ContextMenu("Load")]
     private async void Load()
@@ -42,8 +70,85 @@ public class SheetURLHelper : ScriptableObject
         {
             var result = reader.AsDataSet();
 
-            PrintExcelData(result.Tables);
+    private string GetClassScriptText(System.Data.DataTable table)
+    {
+        // 주석용 열 제거
+        var exceptionColumns = new HashSet<int>();
+        var sheet = table.Rows;
+        for (int i = 0; i < table.Columns.Count; i++)
+        {
+            var name = sheet[DataNameRowNumber][i].ToString();
+            if (IsPassRow(table, i) == true)
+            {
+                exceptionColumns.Add(i);
+            }
         }
+
+        // 스크립트 작성
+        var code = $"public class {table.TableName}\n";
+        code += $"{{\n";
+        for (int i = 0; i < table.Columns.Count; i++)
+        {
+            if (exceptionColumns.Contains(i) == true)
+                continue;
+
+            code += $"\t public {sheet[DataTypeRowNumber][i]} {sheet[DataNameRowNumber][i]};\n";
+        }
+        code += $"}}\n";
+
+        return code;
+    }
+
+    public void ConvertClassToJson(System.Data.DataTable table)
+    {
+        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        Type type = assemblies
+            .Select(a => a.GetType(table.TableName))
+            .FirstOrDefault(t => t != null);
+
+        var datas = new System.Object[table.Rows.Count - DataStartedRowNumber];
+        var filedNames = table.Rows[DataNameRowNumber];
+        for (int i = 0; i < datas.Length; i++)
+        {
+            var data = table.Rows[DataStartedRowNumber + i];
+            try
+            {
+                var instance = Activator.CreateInstance(type);
+                for (int j = 0; j < table.Columns.Count; j++)
+                {
+                    if (IsPassRow(table, j) == true)
+                        continue;
+
+                    var fieldInfo = type.GetField(filedNames[j].ToString());
+                    if (fieldInfo.FieldType.IsEnum == true)
+                    {
+                        Enum.TryParse(fieldInfo.FieldType, data[j].ToString(), out var result);
+                        fieldInfo.SetValue(instance, result);
+                    }
+                    else
+                    {
+                        var value = Convert.ChangeType(data[j].ToString(), fieldInfo.FieldType);
+                        fieldInfo.SetValue(instance, value);
+                    }
+                }
+
+                datas[i] = instance;
+            }
+            catch (ArgumentNullException err)
+            {
+                Debug.LogError($"{table.TableName} 클래스가 생성되지 않음\n\n {err.Message}");
+                return;
+            }
+        }
+
+        var text = JsonConvert.SerializeObject(datas, Formatting.Indented);
+        Debug.Log(text);
+    }
+
+    private bool IsPassRow(System.Data.DataTable table, int index)
+    {
+        return (table.Rows[DataNameRowNumber][index].ToString().Length > 0 && table.Rows[DataNameRowNumber][index].ToString()[0] == '#');
+    }
     }
 
     private void PrintExcelData(System.Data.DataTable table)
