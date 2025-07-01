9569b428f62bc0d38879daa79efef25ebc992c4e
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2025-07-01 23:14:25 +0900

    fix: 잘못된 파일 삭제

--- HateClient/Assets/Scripts/Item.cs
+++
@@ -1,7 +0,0 @@
-public class Item
-{
-	 public ItemType Type;
-	 public string Name;
-	 public string Desc;
-	 public int Price;
-}
--- HateClient/Assets/Scripts/ItemLocalization.cs
+++
@@ -1,7 +0,0 @@
-public class ItemLocalization
-{
-	 public int Type;
-	 public string en;
-	 public string ko;
-	 public string ja;
-}
