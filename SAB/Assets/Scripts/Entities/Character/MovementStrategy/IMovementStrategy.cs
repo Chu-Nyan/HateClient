04535fe9871d9ef2dd6eb8e828f50f6067cc108d
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2025-07-20 16:43:35 +0900

    chore: Character 스크립트 분리

---
+++ SAB/Assets/Scripts/Entities/Character/MovementStrategy/IMovementStrategy.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine.AI;
+
+public interface IMovementStrategy
+{
+    public void Init(NavMeshAgent agent);
+    public void Move();
+    public bool IsMoving { get; }
+}
---
+++ SAB/Assets/Scripts/Entities/Character/MovementStrategy/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : IMovementStrategy
+{
+    private Transform _transform;
+    private NavMeshAgent _agent;
+    private Vector2 _direction;
+
+    public bool IsMoving
+    {
+        get => _direction != Vector2.zero;
+    }
+
+    public void Init(NavMeshAgent agent)
+    {
+        _transform = agent.transform;
+        _agent = agent;
+        InputManager.Instance.RegisterWASDPerformed(SetDirection);
+        InputManager.Instance.RegisterWASDCanceled(SetDirection);
+    }
+
+    public void Move()
+    {
+        var destination = new Vector3(_transform.position.x + _direction.x, _transform.position.y, _transform.position.z + _direction.y);
+        _agent.SetDestination(destination);
+    }
+
+    private void SetDirection(Vector2 dir)
+    {
+        _direction = dir;
+    }
+
+    public void Reset()
+    {
+        InputManager.Instance.UnregisterWASDPerformed(SetDirection);
+    }
+}
