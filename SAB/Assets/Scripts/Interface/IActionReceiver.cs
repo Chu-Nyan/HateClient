6e0abdec1fa2c84769733abd0a5791e663e1295f
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2025-12-01 20:59:04 +0900

    feat: 이동, 행동 인터페이스 추가

---
+++ SAB/Assets/Scripts/Interface/IActionReceiver.cs
@@ -0,0 +1,8 @@
+﻿/// <summary>
+/// 상호작용, 공격 등 행동을 실행하는 객체
+/// </summary>
+public interface IActionReceiver
+{
+    void Attack();
+    void Interact();
+}
---
+++ SAB/Assets/Scripts/Interface/IMovementReceiver.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 입력을 받아 움직이는 객체
+/// </summary>
+public interface IMovementReceiver
+{
+    void Move(Vector3 dir);
+}
