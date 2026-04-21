22a57cdb69b55b48695cb42447e613654f5a0e47
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2026-04-21 20:43:51 +0900

    feat: DB에 CutScene 데이터 추가

---
+++ SAB/Assets/Scripts/Managers/DataBase/CutSceneRepository.cs
@@ -0,0 +1,30 @@
+﻿using SAB.Cutscene;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SAB.DataManger
+{
+    public class CutSceneRepository
+    {
+        public readonly Dictionary<MapType, CutsceneData[]> DataByMapType;
+        public const string FileNameFormat = "{0}_Cutscene";
+
+        public CutSceneRepository()
+        {
+            DataByMapType = new();
+            foreach (MapType type in Enum.GetValues(typeof(MapType)))
+            {
+                CutsceneData[] data = AssetManager.DeserializeJsonSync<CutsceneData[]>(string.Format(FileNameFormat, type.ToString()));
+                if (data == null)
+                {
+                    Debug.LogWarning($"{type} CutScene Data can't find");
+                }
+                else
+                {
+                    DataByMapType.Add(type, data);
+                }
+            }
+        }
+    }
+}
--- SAB/Assets/Scripts/Managers/DataBase/DataBase.cs
+++ SAB/Assets/Scripts/Managers/DataBase/DataBase.cs
@@ -9,12 +9,14 @@ namespace SAB.DataManger
         public readonly SkillRepository SkillRepo;
         public readonly CharacterRepository CharacterRepo;
         public readonly ItemRepository ItemRepo;
+        public readonly CutSceneRepository CutSceneRepo;
 
         public DataBase()
         {
             SkillRepo = new SkillRepository();
             CharacterRepo = new CharacterRepository();
             ItemRepo = new ItemRepository();
+            CutSceneRepo = new CutSceneRepository();
         }
 
         public static  Dictionary<K, V[]> DeserializeArrayByKey<TDTO, K, V>(TDTO[] dtos, Func<TDTO, K> keySelector, Func<TDTO, V> converter)
