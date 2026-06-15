1f6e3d238e8c55bd5fda4e186acbae71aed38008
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2026-06-15 22:14:20 +0900

    feat: Chuengine, 유니티와 Json.NET 호환용 컨버터 추가

---
+++ SAB/Assets/ChuEngine/Utility/Json/JsonWriterExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Chu.Utility.Json
+{
+    public static class JsonWriterExtensions
+    {
+        public static void WriteProperty(this JsonWriter writer, string name, object value)
+        {
+            writer.WritePropertyName(name);
+            writer.WriteValue(value);
+        }
+    }
+}
