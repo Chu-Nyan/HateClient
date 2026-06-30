71157b03b98be19e6028c781407d86cdfe18206e
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2026-06-30 13:22:24 +0900

    feat: ChuEngine.SerializablePair 생성자 추가

--- SAB/Assets/ChuEngine/Utility/SerializablePair.cs
+++ SAB/Assets/ChuEngine/Utility/SerializablePair.cs
@@ -5,7 +5,15 @@ namespace Chu.Utility
     [Serializable]
     public class SerializablePair<T, K>
     {
-        public T Key; 
+        public T Key;
         public K Value;
+
+        public SerializablePair() { }
+
+        public SerializablePair(T key, K value)
+        {
+            Key = key;
+            Value = value;
+        }
     }
 }
