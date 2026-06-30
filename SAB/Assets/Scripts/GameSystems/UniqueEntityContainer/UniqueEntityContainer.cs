c485240071f1b94a3f7d1178fcddd3b1ff97f9dc
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2026-06-30 16:34:49 +0900

    feat: 고유 오브젝트용 컨테이너 추가

---
+++ SAB/Assets/Scripts/GameSystems/UniqueEntityContainer/UniqueEntityContainer.cs
@@ -0,0 +1,26 @@
+﻿using SAB.Cutscene;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniqueEntityContainer
+{
+    private readonly Dictionary<UniqueEntityType, MonoBehaviour> _objectByUniqueType;
+
+    public UniqueEntityContainer()
+    {
+        _objectByUniqueType = new();
+    }
+
+    public void SetUniqueEntity(UniqueEntityType type, MonoBehaviour obj, bool isOverwrite = true)
+    {
+        if (isOverwrite == true)
+            _objectByUniqueType[type] = obj;
+        else
+            _objectByUniqueType.TryAdd(type, obj);
+    }
+
+    public MonoBehaviour GetEntity(UniqueEntityType type)
+    {
+        return _objectByUniqueType[type];
+    }
+}
---
+++ SAB/Assets/Scripts/GameSystems/UniqueEntityContainer/UniqueEntityType.cs
@@ -0,0 +1,7 @@
+﻿namespace SAB.Cutscene
+{
+    public enum UniqueEntityType
+    {
+        Player
+    }
+}
