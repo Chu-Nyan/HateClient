d56719690433c3be538f27e71490a0df9f128c21
Chu-Nyan <141575778+Chu-Nyan@users.noreply.github.com>
2026-07-28 01:56:52 +0900

    chore: 폴더 위치 변경, 불필요한 파일 제거

--- SAB/Assets/Scripts/Commons/Enum.cs
+++ SAB/Assets/Scripts/Commons/Enum.cs
@@ -1,5 +1,35 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+
+public enum ItemType
+{
+    Phone = 10000,
+    Card = 20000,
+    HealingPotion = 10000,
+    ManaPotion = 10001,
+    Elixir = 10002,
+    IronSword = 20000,
+    SteelShield = 20001,
+}
+
+public enum MapType
+{
+    Forest,
+}
+
+public enum FactionType
+{
+    Player = 0,
+    Kingdom,
+    Underworld,
+}
+
+public enum FactionRelation
+{
+    Hostile = -1,
+    Neutral = 0,
+    Friendly = 1
+}
 
 [JsonConverter(typeof(StringEnumConverter))]
 public enum EquipSlot
--- SAB/Assets/Scripts/Commons/Enum/SoundType.cs
+++
@@ -1,7 +0,0 @@
-﻿public enum SoundType
-{
-    // TODO : 자동으로 이름이 추가되는 기능
-    None,
-    SE = 10000,
-    BGM = 20000,
-}
